9e45b17e7b08b9a99b1ac4c32e7d9e3e53b378fd
Слава Конотопец <slavakonotopets@MacBook-Air-Slava.local>
2025-07-08 10:15:36 +0200

    Fix database connection for Render

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a4d0f1bd3ce25f9616bea5a37ede491f3e82005d861c0dfd8b37e54c612c090
-size 8896
+oid sha256:0d81010085d8d3c6bea431480aaf9c80f475f6db2560ecb7a8d6d22adfbf8ee7
+size 8898
